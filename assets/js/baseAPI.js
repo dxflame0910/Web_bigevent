cba950f55d738f05b28e452c51ad7d59b9ac06b0
dongxiaofeng <3090996802@qq.com>
2020-06-27 22:06:41 +0800

    昨晚10点提交login

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,15 +1,8 @@
 $.ajaxPrefilter(function (options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
+    // options.url = 'http://127.0.0.1:8080' + options.url
 })
 
-
-// $.ajaxPrefilter(function (options) {
-//     options.url = "mmm" + options.url
-// })
-
-// $.ajaxPrefilter(function (op) {
-    
-// })
 
 
